e1ca2170ac12828e8f6d923ac3e9bf9bafe6d399
skiddled <66100859+skiddled@users.noreply.github.com>
2024-11-06 23:21:25 -0600

    adding in js for nav menu

--- js/site.js
+++ js/site.js
@@ -1,1 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // Navigation toggle
+  const navToggle = document.querySelector('.nav-toggle');
+  const navMenu = document.querySelector('.nav-menu');
 
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+  });
+});
